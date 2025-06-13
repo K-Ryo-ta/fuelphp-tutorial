01a7a04712723b5351209e7bff85c02ce3ea47ad
K-Ryo-ta <studyprograming51@gmail.com>
2025-06-14 05:10:13 +0900

    ファイルアップロードの方法を学んだ

--- fuel/app/classes/controller/original.php
+++ fuel/app/classes/controller/original.php
@@ -169,6 +169,42 @@ class Controller_Original extends Controller
     return View::forge('form');
   }
 
+  public function action_upload()
+  {
+    if (Input::method() == 'POST') {
+      // このアップロードのカスタム設定
+      $config = array(
+        'path' => DOCROOT . 'files',
+        'randomize' => true,
+        'ext_whitelist' => array('img', 'jpg', 'jpeg', 'gif', 'png'),
+      );
+
+      // $_FILES 内のアップロードされたファイルを処理する
+      Upload::process($config);
+
+      // 有効なファイルがある場合
+      if (Upload::is_valid()) {
+        // 設定にしたがって保存する
+        Upload::save();
+
+        // モデルを呼び出してデータベースを更新する
+        // Model_Uploads::add(Upload::get_files());
+      }
+
+      // エラーを処理する
+      foreach (Upload::get_errors() as $file) {
+        // $file はファイル情報の配列
+        // $file['errors'] は発生したエラーの内容を含む配列で、
+        // 配列の要素は 'error' と 'message' を含む配列
+        foreach ($file['errors'] as $error) {
+          // エラーの内容を表示
+          echo 'Error: ' . $error['error'] . ' - ' . $error['message'] . '<br>';
+        }
+      }
+    }
+    return View::forge('upload');
+  }
+
   /**
    * A typical "Hello, Bob!" type example.  This uses a Presenter to
    * show how to use them.
